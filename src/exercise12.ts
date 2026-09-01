df8216c8c536fbfedcf6ec04c15f7b4a183d1e45
zagog629 <182589426+zagog629@users.noreply.github.com>
2026-09-01 14:41:55 -0400

    exercise12

--- src/exercise12.ts
+++ src/exercise12.ts
@@ -1,3 +1,11 @@
+export type RemoteUser = {
+  id: number;
+  name: string;
+  email: string;
+};
+
 export async function fetchUserEmails(): Promise<string[]> {
-  return [];
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = (await response.json()) as RemoteUser[];
+  return users.map(user => user.email);
 }
