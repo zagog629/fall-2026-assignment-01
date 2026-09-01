9429ce32b23cff5b615e49e0088ddb7cb4446e91
zagog629 <182589426+zagog629@users.noreply.github.com>
2026-09-01 12:38:23 -0400

    exercise08

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,25 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private listeners: {
+    [K in keyof T]?: Array<(data: T[K]) => void>;
+  } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName]!.push(callback);
+  }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    const handlers = this.listeners[eventName];
+    if (!handlers) return;
+    for (const handler of handlers) {
+      handler(data);
+    }
+  }
 }
