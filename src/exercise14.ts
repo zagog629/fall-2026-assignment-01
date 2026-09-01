21551c64a15ca8f00b0f18fb11d5be10ab9f660b
zagog629 <182589426+zagog629@users.noreply.github.com>
2026-09-01 14:46:29 -0400

    exercise14

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,14 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number; 
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const promises = postIds.map(async (id) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  
+  return (await response.json()) as PostItem;
+  });
+  return Promise.all(promises);
 }
