de40765e7cbcfa80e030b07721d7ef22aa7f1275
zagog629 <182589426+zagog629@users.noreply.github.com>
2026-09-01 14:44:05 -0400

    exercise13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,21 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+
+    if (response.status !== 200) {
+      return null;
+    }
+
+    const data = (await response.json()) as TodoItem;
+    return data;
+  } catch (error) {
+    return null;
+  }
 }
